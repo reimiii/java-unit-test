3559c1e3047c89c9ad37548d7011585df08437d2
zero-two <miigo@duck.com>
2023-05-11 13:34:58 +0700

    Assertions

--- src/main/java/franxx/code/test/Calculator.java
+++ src/main/java/franxx/code/test/Calculator.java
@@ -1,6 +1,14 @@
 package franxx.code.test;
 
 public class Calculator {
+    public Integer divide(Integer f, Integer s) {
+        if (s == 0) {
+            throw new IllegalArgumentException("Cannot divide by 0");
+        } else {
+            return f / s;
+        }
+    }
+
     public Integer add(Integer f, Integer s) {
         return f + s;
     }
--- src/test/java/franxx/code/test/CalculatorTest.java
+++ src/test/java/franxx/code/test/CalculatorTest.java
@@ -2,11 +2,30 @@ package franxx.code.test;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
+        assertEquals(20, result);
+//        if (result != 20) {
+//            throw new RuntimeException("Test Gagal");
+//        }
+    }
+
+    @Test
+    public void testDivideSuccess() {
+        var result = calculator.divide(100, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDivideFailed() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(10, 0);
+        });
     }
 }
