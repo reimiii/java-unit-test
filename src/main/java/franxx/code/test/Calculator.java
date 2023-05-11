bde77fe050d0dcd483e80735575dacfac6986ef0
zero-two <miigo@duck.com>
2023-05-11 13:19:00 +0700

    Membuat Test

---
+++ src/main/java/franxx/code/test/Calculator.java
@@ -0,0 +1,7 @@
+package franxx.code.test;
+
+public class Calculator {
+    public Integer add(Integer f, Integer s) {
+        return f + s;
+    }
+}
---
+++ src/test/java/franxx/code/test/CalculatorTest.java
@@ -0,0 +1,12 @@
+package franxx.code.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddSuccess() {
+        var result = calculator.add(10, 10);
+    }
+}
