6ecf5a72a5316f8b4f953578a33ce8dc0c4dbcb2
zero-two <miigo@duck.com>
2023-05-17 20:06:58 +0700

    Test Tags

---
+++ src/test/java/franxx/code/test/SampleIntegrationTest.java
@@ -0,0 +1,20 @@
+package franxx.code.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+    @Test
+    public void test1(){
+
+    }
+
+    @Test
+    public void test2(){
+
+    }
+}
