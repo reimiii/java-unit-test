0c23802dfd225f0700ef3e6705e7dc5261ca94ed
zero-two <miigo@duck.com>
2023-05-18 19:46:22 +0700

    LifeCycle unittest

--- src/test/java/franxx/code/test/OrderedTest.java
+++ src/test/java/franxx/code/test/OrderedTest.java
@@ -1,27 +1,41 @@
 package franxx.code.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+// PER_METHOD is Default
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+    private int counter = 0;
+
+    @BeforeAll
+    void beforeAll() {
+
+    }
+
+    @AfterAll
+    void afterAll() {
+
+    }
+
     @Test
     @Order(3)
     void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(1)
     void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 }
